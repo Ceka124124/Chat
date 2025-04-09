9d2cadc764bb8e0bebb44fd63b2e63e581fd853b
Ceka124124 <rowqa08@gmail.com>
2025-04-09 23:40:56 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCtSf4MPyJktuIX774VogmvsORjMXfClvc",
+    authDomain: "sohbet-uygulamasi-1dec6.firebaseapp.com",
+    databaseURL: "https://sohbet-uygulamasi-1dec6.firebaseio.com",
+    projectId: "sohbet-uygulamasi-1dec6",
+    storageBucket: "sohbet-uygulamasi-1dec6.appspot.com",
+    messagingSenderId: "138065161828"
+};
+firebase.initializeApp(firebaseConfig);
